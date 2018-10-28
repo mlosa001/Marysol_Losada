e6575dcde02bbeb21790ac28f7459fd4508c886e
mlosa001 <mlosa001@gmail.com>
2018-10-27 20:00:58 -0400

    second commit

--- src/org/pursuit/Main.java
+++ src/org/pursuit/Main.java
@@ -4,10 +4,8 @@ package org.pursuit;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         Adventure adventure = new Adventure();
         adventure.startGame();
-//        GuessingGame guessingGame = new GuessingGame();
-//        guessingGame.startGame();
+
     }
 }
